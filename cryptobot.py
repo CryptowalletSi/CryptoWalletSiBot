203405c56c525d9f5b4ecf3fa2958fe879778344
cryptobotdev <cryptobotdev@example.com>
2020-08-07 18:34:53 +0000

    Make some improvements

--- cryptobot.py
+++ cryptobot.py
@@ -8,8 +8,8 @@ import config
 from coin import COINS
 
 
-COMMANDS = ['start', 'balance', 'deposit', 'withdraw', 'tip']
-
+COMMANDS = ['help', 'start', 'balance', 'deposit', 'withdraw', 'tip']
+PUBLIC_COMMANDS = ['help', 'tip']
 
 class Cryptobot:
     def __init__(self, telegram_token=config.TELEGRAM_TOKEN):
@@ -27,17 +27,23 @@ class Cryptobot:
         bot.send_message(chat_id, str(result))
 
     def command(self, bot, update):
-        chat_id = update.message.chat_id
+        msg = update.message
+        chat_id = msg.chat_id
         try:
-            if not update.message.from_user.username:
+            if not msg.from_user.username:
                 raise Exception("You must set a telegram username to use this bot")
-            cmd = update.message.text.split()[0][1:]
+            cmd = msg.text.split()[0][1:]
+            if msg.chat.type != 'private' and cmd not in PUBLIC_COMMANDS:
+                raise Exception(msg.from_user.username + ", that command is only available if you talk to me privately")
             f = getattr(self, 'cmd_' + cmd, None)
             if f:
                 f(bot, update)
         except Exception as e:
             log.exception('command')
             bot.send_message(chat_id, f'Error: {e}')
+
+    def cmd_help(self, bot, update):
+        return self.cmd_start(bot, update)
 
     def cmd_start(self, bot, update):
         chat_id = update.message.chat_id
@@ -48,8 +54,8 @@ class Cryptobot:
         username = msg.from_user.username
         sym = msg.text.split()[1].upper()
         coin = COINS[sym]
-        result = coin.request('listaccounts', [config.MINCONF])
-        self.send_result(bot, update, result)
+        amount = int(coin.request('getbalance', [username, config.MINCONF])['result'])
+        bot.send_message(msg.chat_id, f"You have {amount} {sym}")
 
     def get_addr(self, coin, username):
         result = coin.request('getaddressesbyaccount', [username])
@@ -72,13 +78,17 @@ class Cryptobot:
         sym = msg.text.split()[1].upper()
         coin = COINS[sym]
         toaddr = msg.text.split()[2]
-        amount = coin.request('getbalance', [username, config.MINCONF])['result']
+        amount = int(coin.request('getbalance', [username, config.MINCONF])['result'])
         result = coin.request('sendfrom', [username, toaddr, amount, config.MINCONF])
         self.send_result(bot, update, result)
 
     def cmd_tip(self, bot, update):
+        usage_text = "How to properly tip: /tip @username <amount> <ticker>"
         msg = update.message
         username = msg.from_user.username
+        if not msg.entities[1:]:
+            bot.send_message(msg.chat_id, usage_text)
+            return
         if msg.entities[1].type != 'mention':
             raise Exception("First param must be recipient username")
         recipient = msg.text.split()[1][1:]
